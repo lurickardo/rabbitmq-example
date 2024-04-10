7f6929cb267e4616b0bd3e3dde2c562bd0f42d42
Lurickardo <luizr726@gmail.com>
2024-04-10 02:48:22 -0300

    fix: add more function tests to rabbitmq-example

--- publisher/index.ts
+++ publisher/index.ts
@@ -1,42 +1,47 @@
 import amqp from "amqplib";
 
 async function publishMessage(
-	exchangeName: string,
-	queueName: string,
-	message: any,
+  exchangeName: string,
+  queueName: string,
+  message: any,
+  interval: number = 500,
 ) {
-	try {
-		const connection = await amqp.connect(String(process.env.AMQP_URL));
-		const channel = await connection.createChannel();
+  try {
+    const connection = await amqp.connect(String(process.env.AMQP_URL));
+    const channel = await connection.createChannel();
 
-		await channel.assertExchange(exchangeName, "direct", { durable: true });
-		await channel.assertQueue(queueName, { durable: true });
-		await channel.bindQueue(queueName, exchangeName, "direct");
+    await channel.assertExchange(exchangeName, "direct", { durable: true });
 
-		setInterval(() => {
-			channel.publish(exchangeName, "", Buffer.from(JSON.stringify(message)), {
-				persistent: true,
-			});
-			console.log(
-				`[x] Mensagem publicada na exchange ${exchangeName}.`,
-			);
-		}, 500);
-	} catch (error) {
-		console.error("[!] Ocorreu um erro ao publicar a mensagem:", error);
-	}
+    setInterval(() => {
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true,
+      });
+      console.log(`[x] Mensagem publicada na exchange ${exchangeName}.`);
+    }, interval);
+  } catch (error) {
+    console.error("[!] Ocorreu um erro ao publicar a mensagem:", error);
+  }
 }
 
 const exchangeName = String(process.env.EMAIL_EXCHANGE_NAME);
 const queueName = String(process.env.EMAIL_QUEUE_NAME);
 const messages = {
-	notifications: [
-		{
-			title: "Titulo do texto",
-			recipients: ["luizr726@gmail.com"],
-			body: "Corpo do texto",
-			attachments: [],
-		},
-	],
+  notifications: [
+    {
+      title: "Titulo do texto",
+      recipients: ["luizr726@gmail.com"],
+      body: "Corpo do texto",
+      attachments: [],
+    },
+  ],
 };
 
-publishMessage(exchangeName, queueName, messages);
+publishMessage(exchangeName, queueName, messages, 500);
+
+const message = {
+  title: "Titulo do texto",
+  recipients: ["luizr726@gmail.com"],
+  body: "Corpo do texto",
+  attachments: [],
+};
+publishMessage(exchangeName, "notification", message, 250);
